a82c871aae1c1fc20f33afa9722b8b32fdde220b
Vadim <topilskiy.v1@gmail.com>
2023-09-15 15:24:57 +0300

    Update brain games version 0.3

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,2 +1,15 @@
 import random
 
+
+RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def game_conditions():
+    question = random.randint(0, 100)
+
+    if question % 2 == 0:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+
+    return question, correct_answer
