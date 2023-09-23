67fc6e2ed437b3a01e0fd02b1daf91a9e51c142c
Vadim <topilskiy.v1@gmail.com>
2023-09-23 15:58:51 +0300

    Update brain games version 0.4

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,19 @@
+import random
+
+
+RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(number):
+    if number < 2:
+        return False
+
+    divider = 2
+
+    while divider <= number / 2:
+        if number % divider == 0:
+            return False
+
+        divider += 1
+
+    return True
