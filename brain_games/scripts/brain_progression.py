e69284313a6cc17b6b0b43967f03ea288d3411e1
Vadim <topilskiy.v1@gmail.com>
2023-09-26 16:06:03 +0300

    Update brain games version 0.6

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+RULE = 'What number is missing in the progression?'
 
-from brain_games.engine import launch_game
-from brain_games.games import progression_game
+def create_progression():
+    first_number = random.randint(1, 20)
+    progression = [first_number]
+    progression_length = random.randint(5, 10)
+    gap = random.randint(2, 10)
+
+    while len(progression) <= progression_length:
+        next_number = progression[-1] + gap
+        progression.append(next_number)
+
+    return progression
